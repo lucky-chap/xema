a89d670d8a9648f6a1474c4b0103c8897bd7cba0
Quavo Huncho <wartywarthog7@gmail.com>
2022-02-28 11:18:54 +0000

    Install TailwindCSS and set up 🚀💫

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+	future: {
+		removeDeprecatedGapUtilities: true,
+		purgeLayersByDefault: true,
+	},
+	mode: 'jit',
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			screens: {
+				dark: { raw: '(prefers-color-scheme: dark)' },
+			},
+			animation: {
+				bounce: 'bounce 4s ease-in-out infinite',
+				pulse: 'pulse 4s ease-in-out infinite ',
+			},
+		},
+	},
+	variants: {
+		extend: {
+			display: ['group-hover'],
+		},
+	},
+	fontFamily: {
+		sans: ['Inter', 'sans-serif'],
+		serif: ['Newsreader', 'serif'],
+		mono: ['JetBrains Mono', 'monospace'],
+	},
+	plugins: [],
+}
